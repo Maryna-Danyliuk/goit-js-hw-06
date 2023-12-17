a6a63d00145b26ddb10eb18ec25ebe2666a325b8
Maryna-Danyliuk <106257899+Maryna-Danyliuk@users.noreply.github.com>
2023-12-17 23:56:21 +0200

    Initial commit

---
+++ js/task-4.js
@@ -0,0 +1,49 @@
+getTotalBalanceByGender = (users, gender) => {
+return users = users
+.filter(user => user.gender===gender)
+.reduce((bal, user)=>{
+  return user.balance+bal;
+},0);
+}
+
+ const alUsers = [
+    {
+    name: "Moore Hensley",
+    gender: "male",
+    balance: 2811
+  },
+  {
+    name: "Sharlene Bush",
+    gender: "female",
+    balance: 3821
+  },
+  {
+    name: "Ross Vazquez",
+    gender: "male",
+    balance: 3793
+  },
+  {
+    name: "Elma Head",
+    gender: "female",
+    balance: 2278
+  },
+  {
+    name: "Carey Barr",
+    gender: "male",
+    balance: 3951
+  },
+  {
+    name: "Blackburn Dotson",
+    gender: "male",
+    balance: 1498
+  },
+  {
+    name: "Sheree Anthony",
+    gender: "female",
+    balance: 2764
+  }
+];
+
+console.log(getTotalBalanceByGender(alUsers, "male")); 
+
+console.log(getTotalBalanceByGender(alUsers, "female")); 
